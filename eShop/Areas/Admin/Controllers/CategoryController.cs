44a9d8ee9cbddfde19e2e8105e5746da0abf749d
lnam6507 <lnam6507@gmail.com>
2022-08-25 10:00:27 +0700

    update

--- eShop/Areas/Admin/Controllers/CategoryController.cs
+++ eShop/Areas/Admin/Controllers/CategoryController.cs
@@ -6,9 +6,9 @@ namespace eShop.Areas.Admin.Controllers
 {
     public class CategoryController : BaseController
     {
-        public CategoryController(AppDbContext db): base(db)
+        public CategoryController(AppDbContext db) : base(db)
         {
-            
+
         }
         public IActionResult Index()
         {
@@ -33,6 +33,33 @@ namespace eShop.Areas.Admin.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Update(int id)
+        {
+            var category = _db.Categories.Find(id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Update(int id, Category ct)
+        {
+            var category = _db.Categories.Find(id);
+            category.Name = ct.Name;
+            category.UpdateAt = DateTime.Now;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
+        public IActionResult Delete(int id)
+        {
+            Category ct = _db.Categories.Find(id);
+            _db.Categories.Remove(ct);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var category = _db.Categories.Find(id);
+            return View(category);
+        }
     }
 }
